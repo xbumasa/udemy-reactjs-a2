7d4e480a9f3b184085ef32f1fdeabc742b2387e2
Semion Shulman <shulman.dev@gmail.com>
2020-12-06 00:29:05 +0200

    Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [userInput, setUserInputState] = useState({
+    inputText: ''
+  });
+  
+  const changeUserInput = (event) => {
+    setUserInputState({
+      inputText: event.target.value
+    });
+  }
+  
   return (
     <div className="App">
-    
+      <input
+        type="text"
+        value={userInput.inputText}
+        onChange={changeUserInput} />
+      <p>{userInput.inputText.length}</p>
     </div>
   );
 }
